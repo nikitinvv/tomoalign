4ecd34e7a7e7fc7c877904822f4a9ff6ec988606
math-vrn <vnikitin90@gmail.com>
2019-11-04 11:44:48 -0600

    DEV: add 3Dstd scripts

---
+++ 3Dstd/3Dstd_admm.py
@@ -0,0 +1,126 @@
+import dxchange
+import numpy as np
+import tomocg as tc
+import deformcg as dc
+import scipy as sp
+import sys
+import os
+import matplotlib.pyplot as plt
+
+
+def myplot(u, psi, flow):
+    [ntheta, nz, n] = psi.shape
+
+    plt.figure(figsize=(20, 14))
+    plt.subplot(3, 4, 1)
+    plt.imshow(psi[ntheta//4].real, cmap='gray')
+
+    plt.subplot(3, 4, 2)
+    plt.imshow(psi[ntheta//2].real, cmap='gray')
+    plt.subplot(3, 4, 3)
+    plt.imshow(psi[3*ntheta//4].real, cmap='gray')
+
+    plt.subplot(3, 4, 4)
+    plt.imshow(psi[-1].real, cmap='gray')
+
+    plt.subplot(3, 4, 5)
+    plt.imshow(dc.flowvis.flow_to_color(flow[ntheta//4]), cmap='gray')
+
+    plt.subplot(3, 4, 6)
+    plt.imshow(dc.flowvis.flow_to_color(flow[ntheta//2]), cmap='gray')
+
+    plt.subplot(3, 4, 7)
+    plt.imshow(dc.flowvis.flow_to_color(flow[3*ntheta//4]), cmap='gray')
+    plt.subplot(3, 4, 8)
+    plt.imshow(dc.flowvis.flow_to_color(flow[-1]), cmap='gray')
+
+    plt.subplot(3, 4, 9)
+    plt.imshow(u[nz//2].real)
+    plt.subplot(3, 4, 10)
+    plt.imshow(u[nz//2+nz//8].real)
+
+    plt.subplot(3, 4, 11)
+    plt.imshow(u[:, n//2].real)
+
+    plt.subplot(3, 4, 12)
+    plt.imshow(u[:, :, n//2].real)
+    if not os.path.exists('tmp'+'_'+str(ntheta)+'/'):
+        os.makedirs('tmp'+'_'+str(ntheta)+'/')
+    plt.savefig('tmp'+'_'+str(ntheta)+'/flow'+str(k))
+    plt.clf()
+
+# Update penalty for ADMM
+
+
+def update_penalty(psi, h, h0, rho):
+    # rho
+    r = np.linalg.norm(psi - h)**2
+    s = np.linalg.norm(rho*(h-h0))**2
+    if (r > 10*s):
+        rho *= 2
+    elif (s > 10*r):
+        rho *= 0.5
+    return rho
+
+
+if __name__ == "__main__":
+
+    ndsets=np.int(sys.argv[1])
+    prj = np.load('prjbin2.npy')[0:ndsets*1210,256-64:256+64].astype('complex64')
+    theta = np.load('theta.npy')[0:ndsets*1210]    
+    
+
+    [ntheta,nz,n] = prj.shape  # object size n x,y
+    center = 1173-200 
+    binning = 2
+
+    niter = 64  # tomography iterations
+    pnz = 32  # number of slice partitions for simultaneous processing in tomography
+
+    # data
+    data = prj.copy()
+
+    # initial guess
+    u = np.zeros([nz, n, n], dtype='complex64')
+    psi = data.copy()
+    lamd = np.zeros([ntheta, nz, n], dtype='complex64')
+    flow = np.zeros([ntheta, nz, n, 2], dtype='float32')
+    # optical flow parameters
+    pars = [0.5, 3, 128, 8, 5, 1.1, 0]
+
+    # ADMM solver
+    with tc.SolverTomo(theta, ntheta, nz, n, pnz, center) as tslv:
+        with dc.SolverDeform(ntheta, nz, n) as dslv:
+            rho = 0.5
+            h0 = psi
+            for k in range(niter):
+                # registration
+                flow = dslv.registration_batch(psi, data, flow)
+                # deformation subproblem
+                psi = dslv.cg_deform(data, psi, flow, 4,
+                                     tslv.fwd_tomo_batch(u)+lamd/rho, rho)
+                # tomo subproblem
+                u = tslv.cg_tomo_batch2(psi-lamd/rho, u, 4)
+                h = tslv.fwd_tomo_batch(u)
+                # lambda update
+                lamd = lamd+rho*(h-psi)
+
+                # checking intermediate results
+                myplot(u, psi, flow)
+                if(np.mod(k, 4) == 0):  # check Lagrangian
+                    Tpsi = dslv.apply_flow_batch(psi, flow)
+                    lagr = np.zeros(4)
+                    lagr[0] = np.linalg.norm(Tpsi-data)**2
+                    lagr[1] = np.sum(np.real(np.conj(lamd)*(h-psi)))
+                    lagr[2] = rho*np.linalg.norm(h-psi)**2
+                    lagr[3] = np.sum(lagr[0:3])
+                    print(k, pars[2], np.linalg.norm(flow), rho, lagr)
+                    dxchange.write_tiff_stack(
+                        u.real,  'tmp'+'_'+str(ntheta)+'/rect'+str(k)+'/r', overwrite=True)
+                    dxchange.write_tiff_stack(
+                        psi.real, 'tmp'+'_'+str(ntheta)+'/psir'+str(k)+'/r',  overwrite=True)
+
+                # Updates
+                rho = update_penalty(psi, h, h0, rho)
+                h0 = h
+                pars[2] -= 2
